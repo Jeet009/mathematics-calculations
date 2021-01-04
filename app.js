b016ef1d40eb21d32c1f87cb18c08a1eb9f49f00
Jeet009 <jeetmukherjee009@gmail.com>
2021-01-04 08:34:08 -0500

    First Commit

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
 const express = require("express");
-const percentageRoute = require("./routes/percentage");
-const bodyParser = require("body-parser");
 const app = express();
 
-// Creating Middleware
-
-app.use(bodyParser.json());
-app.use("/home", percentageRoute);
-
-// Listening To The Port
-
 app.listen(8080, () => {
-  console.log("Connecting ... ");
-  console.log("");
-  console.log("Connected To 8080 Port.");
-  console.log("");
-  console.log("Fetching Info ...");
-  console.log("You are using Percentage Calculator API.");
-  console.log("Developed By Jeet Mukerjee");
-  console.log("");
-  console.log("");
-  console.log("Visit On http://localhost:8080/");
-  console.log("About The Developer https://themukherjee.in/");
+  console.log("Connected To Port 8080");
 });
